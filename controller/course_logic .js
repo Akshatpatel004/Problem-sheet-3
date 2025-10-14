0630bc703e92492061adcb6841ad137d90b34c05
Akshat Patel <128491185+Akshatpatel004@users.noreply.github.com>
2025-10-14 22:17:10 +0530

    Add files via upload

--- controller/course_logic .js
+++ controller/course_logic .js
@@ -11,11 +11,19 @@ const CourseLogic = {
         
         req.on("end", async () => {
             try {
-                const courseData = JSON.parse(body);
+                const courseData = await JSON.parse(body);
+                
+                if (!courseData.courseId || !courseData.courseName || !courseData.credits || !courseData.durationMonths || !courseData.department ) {
+                    res.statusCode = 400;
+                    res.setHeader("Content-Type", "application/json");
+                    res.end(JSON.stringify({
+                        "error": "missing key value"
+                    }));
+                    return;
+                }
                 
                 const newCourse = new Course(courseData);
                 const savedCourse = await newCourse.save();
-                
                 res.statusCode = 201;
                 res.setHeader("Content-Type", "application/json");
                 res.end(JSON.stringify({
@@ -30,6 +38,24 @@ const CourseLogic = {
                 }));
             }
         });
+    },
+
+
+    moreCredit: async (req, res, cre) => {
+        try {
+            const morecredit=await Course.find({credits : {$gt:cre}});
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify({
+                "message": "students more than "+cre+" marks",
+                "data :- ":morecredit
+            }))
+        } catch (error) {
+            res.statusCode = 400;
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify({
+                error: error.message
+            })); 
+        }
     }
 };
 
