c2de7f6cc74ea71515a391961f4c09cd776148aa
Akshat Patel <128491185+Akshatpatel004@users.noreply.github.com>
2025-10-15 17:12:14 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import http from 'http';
 import url from 'url';
 const port = 3000;
-import { StudentLogic } from "./controller/student_logic.js"
-import { CourseLogic } from "./controller/course_logic .js"
+import {StudentLogic} from "./controller/student_logic.js"
+import {CourseLogic} from "./controller/course_logic .js"
 import { FacultyLogic } from "./controller/faculty_logic.js"
 import { DepartmentLogic } from "./controller/department_logic.js"
+import { LibraryLogic } from "./controller/library_logic.js"
 import { asyncTryCatch } from './Utils/async_try_catch.js';
 import { connectDB } from './DB/mongodb.js';
 
@@ -18,34 +19,51 @@ const server = http.createServer(async (req, res) => {
     console.log(`current path : - ${path}`);
     res.setHeader("Content-Type", "application/json");
 
+    // student
     if (path == "/") {
         res.end("this is home page");
-    } else if (path == "/api/students/all" && req.method === "GET") {
-        asyncTryCatch(StudentLogic.fetch_all(req, res));
-    } else if (path == "/api/students/insert" && req.method === "POST") {
-        asyncTryCatch(StudentLogic.insertStudent(req, res));
-    } else if (path === "/api/students/findcourse" && req.method === "POST") {
+    }else if(path == "/api/students/all" && req.method==="GET"){
+        asyncTryCatch(StudentLogic.fetch_all(req,res));
+    }else if(path == "/api/students/insert" && req.method==="POST"){
+        asyncTryCatch(StudentLogic.insertStudent(req,res));
+    }else if(path === "/api/students/findcourse" && req.method==="POST"){
         if (querry.course) {
-            asyncTryCatch(StudentLogic.student_findcourse(req, res, querry.course));
+            asyncTryCatch(StudentLogic.student_findcourse(req,res,querry.course));
         } else {
             return
         }
-    } else if (path === "/api/students/moremarks" && req.method === "POST") {
+    }else if(path === "/api/students/moremarks" && req.method==="POST"){
         if (querry.mark) {
-            asyncTryCatch(StudentLogic.more_marks(req, res, querry.mark));
+            asyncTryCatch(StudentLogic.more_marks(req,res,querry.mark));
         } else {
             return
         }
-    } else if (path === "/api/students/updateMark" && req.method === "POST") {
+    }else if(path === "/api/students/updateMark" && req.method==="POST"){
         if (querry.course && querry.mark) {
-            asyncTryCatch(StudentLogic.update_allStudentMark(req, res, querry.course, querry.mark));
+            asyncTryCatch(StudentLogic.update_allStudentMark(req,res,querry.course, querry.mark));
         } else {
             return
         }
     }
     // course
-    else if (path == "/course" && req.method === "POST") {
-        asyncTryCatch(CourseLogic.insertCourse(req, res));
+    else if(path == "/api/course/insert" && req.method==="POST"){
+        asyncTryCatch(CourseLogic.insertCourse(req,res));
+    } else if(path == "/api/course/fetchall" && req.method==="GET"){
+        asyncTryCatch(CourseLogic.fetch_all(req,res));
+    }else if(path == "/api/course/moreCredit" && req.method==="POST"){
+        if (querry.credit ) {
+            asyncTryCatch(CourseLogic.moreCredit(req,res,querry.credit));
+        } else {
+            return
+        }
+    }else if(path == "/api/course/duration_sort" && req.method==="GET"){
+        asyncTryCatch(CourseLogic.duration_sort(req,res));
+    } else if(path == "/api/course/rename_collection" && req.method==="PUT"){
+        if (querry.oldname && querry.newname) {
+            asyncTryCatch(CourseLogic.rename_collection(req,res,querry.oldname,querry.newname));
+        } else {
+            return
+        }
     }
     // faculty
     else if (path == "/api/faculty/all" && req.method === "GET") {
@@ -73,6 +91,24 @@ const server = http.createServer(async (req, res) => {
         asyncTryCatch(DepartmentLogic.findSmallOrBlockA(req, res));
     } else if (path == "/api/department/name-courses" && req.method === "GET") {
         asyncTryCatch(DepartmentLogic.findAllDeptNameAndCourses(req, res));
+    }
+    // library
+    else if (path == "/api/Library/all" && req.method === "GET") {
+        asyncTryCatch(LibraryLogic.fetch_all(req, res));
+    } else if (path == "/api/Library/insert" && req.method === "POST") {
+        asyncTryCatch(LibraryLogic.insertLibrary(req, res));
+    } else if (path == "/api/Library/FindCategory" && req.method === "GET") {
+        if (querry.category) {
+            asyncTryCatch(LibraryLogic.FindCategory(req, res,querry.category));
+        } else {
+            return
+        }
+    } else if (path == "/api/Library/deleteCollection" && req.method === "DELETE") {
+        if (querry.collectionName) {
+            asyncTryCatch(LibraryLogic.DeleteCollection(req, res,querry.collectionName));
+        } else {
+            return
+        }
     }
 });
 
